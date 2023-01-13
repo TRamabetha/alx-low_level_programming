2d441145ecd7904cc31a7135f7326e9615055301
Tr_jik <tsheporamabetha@gmail.com>
2023-01-13 14:05:22 +0200

    main opodes update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,49 +1,24 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- * @n: number of bytes to print
- *
- * Return: void
- */
-void print_opcodes(char *a, int n)
-{
-int i;
+int main(int argc, char *argv[]) {
+    int num_bytes, i;
+    char *main_func = (char *)main;
 
-for (i = 0; i < n; i++)
-{
-printf("%.2hhx", a[i]);
-if (i < n - 1)
-printf(" ");
-}
-printf("\n");
+    if (argc != 2) {
+        printf("Error\n");
+        exit(1);
+    }
 
-}
+    num_bytes = atoi(argv[1]);
+    if (num_bytes < 0) {
+        printf("Error\n");
+        exit(2);
+    }
 
-/**
- * main - prints the opcodes of its own main function
- * @argc: argument count
- * @argv: argument vector
- *
- * Return: O
- */
-int main(int argc, char **argv)
-{
-int a;
+    for (i = 0; i < num_bytes; i++)
+        printf("%.2x", (unsigned char)main_func[i]);
+    printf("\n");
 
-if (argc != 2)
-{
-printf("Error\n");
-exit(1);
-}
-a = atoi(argv[1]);
-if (a < 0)
-{
-printf("Error\n");
-exit(2);
-}
-print_opcodes((char *)&main, a);
-return (0);
+    return (0);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,32 +1,23 @@
 #include "3-calc.h"
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-/**
- * main - ...
- * @argc: ...
- * @argv: ...
- *
- * Return: ...
- */
-int main(int argc, char *argv[])
-{
-	int (*oprt)(int, int);
+int main(int argc, char *argv[]) {
+    int num1, num2;
+    int (*f)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4) {
+        printf("Error\n");
+        exit(98);
+    }
 
-	oprt = get_op_func(argv[2]);
-
-	if (!oprt)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-	return (0);
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    f = get_op_func(argv[2]);
+    if (f == NULL) {
+        printf("Error\n");
+        exit(99);
+    }
+    printf("%d\n", f(num1, num2));
+    return (0);
 }
