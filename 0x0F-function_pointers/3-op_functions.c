d1bb2127f12b9632a1ce00ad7d22fa41f9064cb0
Tr_jik <tsheporamabetha@gmail.com>
2023-01-13 13:53:00 +0200

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,27 +1,33 @@
 #include "3-calc.h"
 #include <stdio.h>
 
-int op_add(int a, int b) {
+int op_add(int a, int b) 
+{
     return a + b;
 }
 
-int op_sub(int a, int b) {
+int op_sub(int a, int b) 
+{
     return a - b;
 }
 
-int op_mul(int a, int b) {
+int op_mul(int a, int b) 
+{
     return a * b;
 }
 
-int op_div(int a, int b) {
-    if (b == 0) {
+int op_div(int a, int b) 
+{
+    if (b == 0) 
+	{
         printf("Error\n");
         exit(100);
     }
     return a / b;
 }
 
-int op_mod(int a, int b) {
+int op_mod(int a, int b) 
+{
     if (b == 0) {
         printf("Error\n");
         exit(100);
