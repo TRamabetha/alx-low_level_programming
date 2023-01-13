53b58a376b783fcaaadc6ca0fad39f48c4d52e0f
Tr_jik <tsheporamabetha@gmail.com>
2023-01-13 14:10:58 +0200

    Update 100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,24 +1,47 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main(int argc, char *argv[]) {
-    int num_bytes, i;
-    char *main_func = (char *)main;
+/**
+ * print_opcodes - print the opcodes of this program
+ * @a: address of the main function
+ * @n: number of bytes to print
+ *
+ * Return: void
+ */
+void print_opcodes(char *a, int n)
+{
+int i;
 
-    if (argc != 2) {
-        printf("Error\n");
-        exit(1);
-    }
+for (i = 0; i < n; i++)
+{
+printf("%.2x", (unsigned char)main_func[i]);
+}
+printf("\n");
 
-    num_bytes = atoi(argv[1]);
-    if (num_bytes < 0) {
-        printf("Error\n");
-        exit(2);
-    }
+}
 
-    for (i = 0; i < num_bytes; i++)
-        printf("%.2x", (unsigned char)main_func[i]);
-    printf("\n");
+/**
+ * main - prints the opcodes of its own main function
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: O
+ */
+int main(int argc, char **argv)
+{
+int a;
 
-    return (0);
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+num_bytes = atoi(argv[1]);
+if (num_bytes < 0) 
+{
+printf("Error\n");
+exit(2);
+}
+print_opcodes((char *)&main, num_bytes);
+return (0);
 }
