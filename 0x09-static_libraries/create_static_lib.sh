5b37ffb9cb39775df4176b1ee2a9ac55e8a5d83d
Tr_jik <84567451+TRamabetha@users.noreply.github.com>
2023-01-16 14:41:02 +0200

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
+# Create an empty archive file
+ar -rcs liball.a
+
+# Add all .c files to the archive
+for file in *.c; do
+    ar -r liball.a $file
+done
